8834aabe70f883c6bff747426d8bb469e483080e
tako <tako@XBMC.(none)>
2012-03-06 23:35:19 +0100

    Trying a different absolute path for the hdhomerun lib on Macs

--- plugin.video.hdhomerun/hdhomerun.py
+++ plugin.video.hdhomerun/hdhomerun.py
@@ -118,7 +118,7 @@ class LibHdhr:
         if (sys.platform == 'linux2'):
             lib = self._libhdhr = cdll.LoadLibrary("libhdhomerun.so.1.0.0")
         elif (sys.platform == 'darwin'):
-            lib = self._libhdhr = cdll.LoadLibrary("/Applications/XBMC.app/Contents/Frameworks/libhdhomerun.dylib")
+            lib = self._libhdhr = cdll.LoadLibrary("/Applications/hdhomerun_config_gui.app/Contents/Frameworks/libhdhomerun.dylib")
         else:
             lib = self._libhdhr = cdll.LoadLibrary("libhdhomerun.dll")
         # Discovery related functions
